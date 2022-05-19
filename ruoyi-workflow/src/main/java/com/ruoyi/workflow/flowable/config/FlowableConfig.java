23ababad23fd342b6151ed4d105d6c0fac18f20d
sgs98 <1742057357@qq.com>
2022-05-19 15:00:36 +0800

    添加流程自定义生成id

--- ruoyi-workflow/src/main/java/com/ruoyi/workflow/flowable/config/FlowableConfig.java
+++ ruoyi-workflow/src/main/java/com/ruoyi/workflow/flowable/config/FlowableConfig.java
@@ -1,10 +1,18 @@
 
 package com.ruoyi.workflow.flowable.config;
 
+import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.util.IdUtil;
+import org.flowable.common.engine.impl.cfg.IdGenerator;
 import org.flowable.spring.SpringProcessEngineConfiguration;
 import org.flowable.spring.boot.EngineConfigurationConfigurer;
 import org.springframework.context.annotation.Configuration;
-
+/**
+ * @program: ruoyi-vue-plus
+ * @description: 配置
+ * @author: gssong
+ * @created: 2021/10/03 19:31
+ */
 @Configuration
 public class FlowableConfig implements EngineConfigurationConfigurer<SpringProcessEngineConfiguration> {
 
@@ -20,6 +28,14 @@ public class FlowableConfig implements EngineConfigurationConfigurer<SpringProce
         processEngineConfiguration.setAnnotationFontName("宋体");
         processEngineConfiguration.setLabelFontName("宋体");
         processEngineConfiguration.setProcessDiagramGenerator(new CustomDefaultProcessDiagramGenerator());
+
+        /**
+         * 自定义id
+         */
+        processEngineConfiguration.setIdGenerator(() -> {
+            Snowflake snowflake = IdUtil.getSnowflake(1, 1);
+            return snowflake.nextIdStr();
+        });
     }
 }
 
